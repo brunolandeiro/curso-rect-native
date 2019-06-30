8b319163e2861c12192f8a8f5228b72534ae2109
Bruno Landeiro <landeiro.b@gmail.com>
2019-06-30 13:37:23 -0300

    exercicios

--- exercicios/src/Menu.js
+++ exercicios/src/Menu.js
@@ -4,8 +4,26 @@ import Simples from './componentes/Simples';
 import ParImpar from './componentes/ParImpar'
 import Contador from './componentes/Contador'
 import NerdCast from './componentes/NerdCast';
+import {Avo} from './componentes/ComunicacaoDireta';
+import ListaFlex from './componentes/ListaFlex';
+import TextoSincronizado from './componentes/ComunicacaoIndireta';
+import Flex from './componentes/Flex';
 
 export default createDrawerNavigator({
+    Flex: {
+        screen: Flex
+    },
+    ListaFlex: {
+        screen: ListaFlex,
+        navigationOptions: { title: 'Lista (Flex Box)' }
+    },
+    TextoSincronizado: {
+        screen: TextoSincronizado,
+        navigationOptions: { title: 'Texto Sincronizado' }
+    },
+    Avo: {
+        screen: () => <Avo nome='Jozé' sobrenome='Silva' />
+    },
     NerdCast:{
         screen: () => <NerdCast />
     },
